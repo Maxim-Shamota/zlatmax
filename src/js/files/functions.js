631ecc17eae32533c54c4f2aa263ebe470474cf1
Maxim <xbultazar@gmail.com>
2022-03-12 15:27:39 +0300

    finished menu

--- src/js/files/functions.js
+++ src/js/files/functions.js
@@ -416,6 +416,12 @@ export function menuInit() {
 			if (bodyLockStatus && e.target.closest('.icon-menu')) {
 				bodyLockToggle();
 				document.documentElement.classList.toggle("menu-open");
+				if (document.documentElement.classList.contains("catalog-open")) {
+					document.documentElement.classList.remove("catalog-open");
+				}
+				if (document.documentElement.classList.contains("_sub-menu-open")) {
+					document.documentElement.classList.remove('_sub-menu-open');
+				}
 			}
 		});
 	};
--- src/js/files/script.js
+++ src/js/files/script.js
@@ -19,7 +19,6 @@ function documentActions(e) {
     if (targetElement.closest('[data-parent]')) {
         const subMenuId = targetElement.dataset.parent ? targetElement.dataset.parent : null;
         const subMenu = document.querySelector(`[data-submenu="${subMenuId}"]`);
-        const catalogMenu = document.querySelector('.catalog-header');
 
         if (subMenu) {
             const activeLink = document.querySelector('._sub-menu-active');
@@ -28,7 +27,9 @@ function documentActions(e) {
             if (activeLink && activeLink !== targetElement) {
                 activeLink.classList.remove('_sub-menu-active');
                 activeBlock.classList.remove('_sub-menu-open');
+                document.documentElement.classList.remove('_sub-menu-open');
             } 
+            document.documentElement.classList.toggle('_sub-menu-open');
             targetElement.classList.toggle('_sub-menu-active');
             subMenu.classList.toggle('_sub-menu-open');
             
@@ -38,4 +39,27 @@ function documentActions(e) {
 
         e.preventDefault();
     }
-}
+
+    if (targetElement.closest('.menu-top-header__link_catalog')) {
+        // const catalogLink = targetElement.closest('.menu-top-header__link_catalog');
+        document.documentElement.classList.add('catalog-open');
+        e.preventDefault();
+    }
+    
+    if (targetElement.closest('.menu-catalog__back')) {
+        document.documentElement.classList.remove('catalog-open');
+
+        document.querySelector('._sub-menu-active') ? document.querySelector('._sub-menu-active').classList.remove('_sub-menu-active') : null;
+        document.querySelector('._sub-menu-open') ? document.querySelector('._sub-menu-open').classList.remove('_sub-menu-open') : null;
+        // document.documentElement.classList.remove('sub-catalog-open');
+        e.preventDefault();
+    }
+
+    if (targetElement.closest('.sub-menu-catalog__back')) {
+        document.documentElement.classList.remove('_sub-menu-open');
+        document.querySelector('._sub-menu-active') ? document.querySelector('._sub-menu-active').classList.remove('_sub-menu-active') : null;
+        document.querySelector('._sub-menu-open') ? document.querySelector('._sub-menu-open').classList.remove('_sub-menu-open') : null;
+        // document.documentElement.classList.remove('sub-catalog-open');
+        e.preventDefault();
+    }
+ }
